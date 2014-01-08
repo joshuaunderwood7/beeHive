ffa8d59c8ae15e8657051129ec3ca677520da239
Joshua Underwood <joshuaunderwood7@gmail.com>
2014-01-08 08:28:06 -0700

    Added undo feature.  Error when deleting all of the lines

--- FileMan.hs
+++ FileMan.hs
@@ -5,7 +5,6 @@ readFileToBuffer :: String -> (String -> b) -> IO b
 readFileToBuffer filename inputFunc = do 
             input <- readFile filename
             return $ inputFunc input
-            
 
 writeBufferToFile :: String -> String -> IO ()
 writeBufferToFile filename output = writeFile  filename output
--- Hed.hs
+++ Hed.hs
@@ -26,9 +26,15 @@ fillDocument :: String -> Document
 fillDocument input = Buffer 1 [] (head readin) (tail readin)
     where readin = lines input
 
-insertLine :: Document -> [Char] -> Document
-insertLine (Buffer lineNum prev cur post) newline = 
+appendLine :: Document -> [Char] -> Document
+appendLine (Buffer 0 [] [] []) newline = Buffer 1 [] newline [] 
+appendLine (Buffer lineNum prev cur post) newline = 
                 Buffer (lineNum + 1) (cur : prev) newline post
+
+insertLine :: Document -> [Char] -> Document
+insertLine (Buffer 0 [] [] []) newline = Buffer 1 [] newline [] 
+insertLine (Buffer lineNum prev cur post) newline = 
+                Buffer lineNum prev newline (cur : post)
 
 deleteLine :: Document -> Document
 deleteLine (Buffer 0 _  _ _ )   = Buffer 0 [] [] []
@@ -70,6 +76,7 @@ executeCommand command doc
     | abriv == 'g' && args == 1 = gotoLine doc (((read.head.(drop 1).words) command)::Int )
     | abriv == 'r' && args >= 1 = replaceLine doc stament
     | abriv == 'i' && args >= 1 = insertLine doc stament
+    | abriv == 'a' && args >= 1 = appendLine doc stament
     | abriv == 's' && args == 2 = replaceSubLine doc ((head.words) stament) ((head.(drop 1).words) stament)
     | otherwise    = doc
         where abriv   = (head.head.words) command
@@ -91,7 +98,7 @@ insertMode input = do
         ".." -> input
         _    -> do
             doc <- input
-            insertMode $ return $ insertLine doc line
+            insertMode $ return $ appendLine doc line
 
 visualMode :: IO Document -> IO Document
 visualMode input = do
@@ -101,33 +108,39 @@ visualMode input = do
     putStr $ "V " ++ (show line) ++ " > "
     putStr $partToString doc line line   
     hFlush stdout
+    hSetEcho stdin False
     command <- getChar
+    hSetEcho stdin True
     case (command) of
         'j' -> return (executeCommand ("g " ++ (show (line + 1))) doc) >>= visualMode.return
         'k' -> return (executeCommand ("g " ++ (show (line - 1))) doc) >>= visualMode.return
         'q' -> do 
             hSetBuffering stdin LineBuffering 
             input
+        _   -> putStrLn "" >> visualMode input
 
 
-progLoop :: IO Document -> IO Document 
+progLoop :: IO [Document] -> IO Document 
 progLoop input = do
-    command <- input >>= promptLine
+    command <- input >>= promptLine.head
     if command /= "" then 
         case (head command) of
-            'Q' -> input
-            'V' -> input >>= (putStrLn.allToString) >> progLoop input
+            'Q' -> fmap head input
+            'V' -> input >>= (putStrLn.allToString.head) >> progLoop input
             'v' ->  catch ( do
                 doc <-input
                 let ltop = ((read.head.(drop 1).words) command) :: Int 
                 let lbot = ((read.head.(drop 2).words) command) :: Int 
-                putStrLn $ partToString doc ltop lbot 
+                putStrLn $ partToString (head doc) ltop lbot 
                 progLoop input ) (handleError (progLoop input)) 
-            '.' -> insertMode input >>= progLoop.return 
-            'n' -> visualMode input >>= progLoop.return
+            '.' -> do { doc <- input; insertMode (fmap head input) >>= (\x -> (progLoop.return) $ x:doc) }
+            'n' -> do { doc <- input; visualMode (fmap head input) >>= (\x -> (progLoop.return) $ x:doc) }
+            'u' -> do 
+                doc <- input 
+                if ((length doc) > 1) then progLoop $ fmap tail input else progLoop input
             _   -> do
                 doc <- input 
-                catch ( progLoop $ return $ executeCommand command doc) 
+                catch ( progLoop $ return $ (executeCommand command (head doc) : doc ) )  
                       ( handleError (progLoop input) ) 
     else progLoop input
 
@@ -158,7 +171,7 @@ main = do
     fileName <- getArgs >>= return.getFilename'
     print fileName
 
-    progLoop $ readFileToBuffer fileName fillDocument
+    progLoop $ readFileToBuffer fileName fillDocument >>= (\x -> return (x:[]))
 
     --print "saving test.txt"
     --(writeBufferToFile "test.txt").allToString --The document--
@@ -167,3 +180,5 @@ main = do
 
     --textInput ""
 
+--Now this is the proper way to implement ed in Haskell
+--main = getLine >> putStrLn "?" >> main
